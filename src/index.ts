d4d34b9465cea1b84bf1e89e4eb65716542d10dc
lolicon <lolicon@mail.az>
2022-01-06 11:46:54 +0800

    feat: polish sample code

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,6 @@ import { prettyprint } from './Describe'
 
 setImmediate(() =>
   Java.perform(() => {
-    // const ContextWrapper = Java.use('android.content.ContextWrapper')
-    // ContextWrapper.attachBaseContext.implementation = function (
-    //   ...args: any[]
-    // ) {
-    //   console.log('\n')
-    //   console.error(args)
-    //   console.log('=====vs=====')
-    //   console.log(JSON.stringify(prettyprint(args, 3, 3), null, 2))
-    //   return this.attachBaseContext(...args)
-    // }
-
     const Activity = Java.use('android.app.Activity')
     Activity.onCreate.overloads.forEach((overload: any) => {
       overload.implementation = function (...args: any[]) {
